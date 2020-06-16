7de5d2efea9a3ea16994c0631b241348504cde2f
Oleksandr Biliaiev <Oleksandr_Biliaiev@epam.com>
2020-06-16 20:11:09 +0300

    remove console err & adding about info

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
-weatherForm.addEventListener('submit', (e) => {
+weatherForm && weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     if (!location) {
